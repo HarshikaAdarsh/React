a014d68857e36df053f614bcc6e5b29de1582431
HarshikaAdarsh <92363685+HarshikaAdarsh@users.noreply.github.com>
2023-07-20 23:05:48 +0530

    listen handelEvent

--- p2using_an_API/src/components/SearchBar.js
+++ p2using_an_API/src/components/SearchBar.js
@@ -1,9 +1,14 @@
 function SearchBar(){
+    const handleFormSubmit = () => {
+       console.log('I need to tell the parents sbout some data');
+    };
    
-    return (<div>
-        <input/>
-       
-      </div>
+    return (
+    <div>
+        <form onSubmit={handleFormSubmit}>
+             <input/>
+        </form>
+    </div>
     );
     
 }
